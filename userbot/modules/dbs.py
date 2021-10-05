81f61f1b6a6f89b751e0aa434c55b2c9af4277be
ᴷᵞᵞ <89422473+muhammadrizky16@users.noreply.github.com>
2021-10-06 02:10:33 +0700

    Update dbs.py

--- userbot/modules/dbs.py
+++ userbot/modules/dbs.py
@@ -29,5 +29,5 @@ async def amireallydbs(dbs):
     await dbs.edit(""
                    f"**User :** `{DEFAULTUSER}` \n"
                    f"**Status Database:** `{db}`\n"
-                   f"**Rose-Userbot :** `{BOT_VER}`"
+                   f"**Kyy-Userbot :** `{BOT_VER}`"
                    "")
