113b6b6ea451c4ce0c6a1d944b98d8a7bb8360a8
ᴷᵞᵞ <89422473+muhammadrizky16@users.noreply.github.com>
2021-10-05 23:22:49 +0700

    Create salam.py

---
+++ userbot/modules/salam.py
@@ -0,0 +1,48 @@
+from platform import uname
+from userbot import ALIVE_NAME, CMD_HELP
+from userbot.events import register
+
+# ================= CONSTANT =================
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
+# ============================================
+
+
+@register(outgoing=True, pattern='^P(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("𝐀𝐬𝐬𝐚𝐥𝐚𝐦𝐮'𝐚𝐥𝐚𝐢𝐤𝐮𝐦...")
+
+
+@register(outgoing=True, pattern='^.atg(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("𝐀𝐒𝐓𝐀𝐆𝐇𝐅𝐈𝐑𝐔𝐋𝐋𝐀𝐇....SAYANG!!!!")
+
+
+@register(outgoing=True, pattern='^L(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("𝐖𝐚'𝐚𝐥𝐚𝐢𝐤𝐮𝐦𝐬𝐚𝐥𝐚𝐦...")
+
+
+@register(outgoing=True, pattern='^.ast(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("𝐀𝐒𝐓𝐀𝐆𝐇𝐅𝐈𝐑𝐔𝐋𝐋𝐀𝐇......")
+
+
+CMD_HELP.update({
+    "salam": 
+    "P\
+\nUsage: Untuk Memberi salam.\
+\n\nL\
+\nUsage: Untuk Menjawab Salam."
+})
+
+CMD_HELP.update({
+    "salam2": 
+    ".atg\
+\nUsage: Istighfar 1.\
+\n\n.ast\
+\nUsage: Istighfaf 2."
+})
