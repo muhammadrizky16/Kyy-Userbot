523a38c3c9f17e98d74e8ba60e2de3b30b6bb8cb
muhammadrizky16 <muhammadrizky16.malikkudus369@gmail.com>
2021-10-06 02:42:12 +0000

    pylint: auto fixes
    Signed-off-by: muhammadrizky16 <muhammadrizky16.malikkudus36o@gmail.com>

--- userbot/modules/__help.py
+++ userbot/modules/__help.py
@@ -4,8 +4,8 @@ from userbot import BOT_USERNAME
 from userbot.events import register
 
 logging.basicConfig(
-    format="[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s", level=logging.WARNING
-)
+    format="[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s",
+    level=logging.WARNING)
 
 
 @register(outgoing=True, pattern=r"^\.helpme")
