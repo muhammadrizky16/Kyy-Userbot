72f5d85152be4835324e34f6b292cd8345cfd6a0
ᴷᵞᵞ <89422473+muhammadrizky16@users.noreply.github.com>
2021-10-07 05:37:27 +0700

    Update toxic.py

--- userbot/modules/toxic.py
+++ userbot/modules/toxic.py
@@ -1,6 +1,10 @@
-
-from userbot import CMD_HELP
+from platform import uname
+from userbot import ALIVE_NAME, CMD_HELP
 from userbot.events import register
+
+# ================= CONSTANT =================
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
+# ============================================
 
 
 @register(outgoing=True, pattern='^.d(?: |$)(.*)')
