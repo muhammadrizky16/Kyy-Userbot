49947fba7959c69e0f975e3105fc81eb2ceb91e4
muhammadrizky16 <malikkudus369@gmail.com>
2021-10-06 22:02:14 +0000

    pylint: auto fixes
    Signed-off-by: muhammadrizky16 <malikkudus369@gmail.com>

--- userbot/modules/kyyubot.py
+++ userbot/modules/kyyubot.py
@@ -114,7 +114,7 @@ async def typewriter(typew):
     await typew.edit("`Ayukkk Ewean Ayanggg🤤`")
 
 
-@register(outgoing = True, pattern="^.tittle(?: |$)(.*)")
+@register(outgoing=True, pattern="^.tittle(?: |$)(.*)")
 async def typewriter(typew):
     typew.pattern_match.group(1)
     await typew.edit("**OI ANAK TITLE**")
