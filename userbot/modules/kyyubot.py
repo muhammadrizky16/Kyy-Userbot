a6e979ba380b328e6f1c9481b1ea68360322b8bc
muhammadrizky16 <malikkudus369@gmail.com>
2021-10-06 21:54:03 +0000

    pylint: auto fixes
    Signed-off-by: muhammadrizky16 <malikkudus369@gmail.com>

--- userbot/modules/kyyubot.py
+++ userbot/modules/kyyubot.py
@@ -104,7 +104,7 @@ async def typewriter(typew):
     await typew.edit("Lari Ada Plastik KePanasan")
 
 
-@ register(outgoing = True, pattern="^.ange(?: |$)(.*)")
+@ register(outgoing=True, pattern="^.ange(?: |$)(.*)")
 async def typewriter(typew):
     typew.pattern_match.group(1)
     await typew.edit("`Ayanggggg😖`")
@@ -114,7 +114,7 @@ async def typewriter(typew):
     await typew.edit("`Ayukkk Ewean Ayanggg🤤`")
 
 
-@ register(outgoing = True, pattern="^.tittle(?: |$)(.*)")
+@ register(outgoing=True, pattern="^.tittle(?: |$)(.*)")
 async def typewriter(typew):
     typew.pattern_match.group(1)
     await typew.edit("**OI ANAK TITLE**")
@@ -169,4 +169,4 @@ CMD_HELP.update({
     \n↳ : Sangean\
     \n\n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.tittle`\
     \n↳ : ngatain bocah gila tittle."
-    })
+})
