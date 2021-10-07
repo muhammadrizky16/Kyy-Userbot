86ac152d6634f5097aa6f4c54bdaf124794f69d3
ᴷᵞᵞ <89422473+muhammadrizky16@users.noreply.github.com>
2021-10-07 09:59:03 +0700

    Create followme.py

---
+++ userbot/modules/followme.py
@@ -0,0 +1,24 @@
+""" Userbot module for other small commands. """
+from userbot import CMD_HELP, ALIVE_NAME
+from userbot.events import register
+
+
+# ================= CONSTANT =================
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
+
+
+@register(outgoing=True, pattern="^.follow$")
+async def usit(e):
+    await e.edit(
+        "✨ **Kʏʏ-Usᴇʀʙᴏᴛ** ✨ \n"
+        "➥ **Teleram :** [ҡʏʏ](t.me/IDnyaKosong)\n"
+        "➥ **Repo :** [ɢɪᴛʜᴜʙ](https://github.com/muhammadrizky16/)\n"
+        "➥ **Instagram :** [ɪɴsᴛᴀɢʀᴀᴍ](instagram.com/rizkyhamdanii16_)\n"
+        "➥ **Groups :** [ɢʀᴏᴜᴘs](https://t.me/sinibrokk)\n"
+        "➥ **Channel :** [ᴄʜᴀɴɴᴇʟ](https://t.me/ahhsudahlahhh)")
+
+
+CMD_HELP.update({
+  "followme": "`.follow`\
+  \nUsage: Difollow dan Join tod."
+})
