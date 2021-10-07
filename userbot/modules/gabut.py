15633563f18a047690745e0ee6281073bf772731
ᴷᵞᵞ <89422473+muhammadrizky16@users.noreply.github.com>
2021-10-07 10:09:30 +0700

    Create gabut.py

---
+++ userbot/modules/gabut.py
@@ -0,0 +1,69 @@
+from platform import uname
+from userbot import ALIVE_NAME, CMD_HELP
+from userbot.events import register
+
+# ================= CONSTANT =================
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
+# ============================================
+
+
+@register(outgoing=True, pattern="^.g(?: |$)(.*)")
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit(f"**JAKA SEMBUNG BAWA GOLOK**")
+    sleep(3)
+    await typew.edit("**NIMBRUNG LAH GOBLOKK!!!**")
+# Pantun
+
+
+@register(outgoing=True, pattern="^.p(?: |$)(.*)")
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("`Salam Dulu Biar Sopan...`")
+    sleep(2)
+    await typew.edit("`السَّلاَمُ عَلَيْكُمْ وَرَحْمَةُ اللهِ وَبَرَكَاتُهُ`")
+# Salam
+
+
+@register(outgoing=True, pattern="^.l(?: |$)(.*)")
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("`Kalo Orang Salam Itu Dijawab...`")
+    sleep(2)
+    await typew.edit("`وَعَلَيْكُمْ السَّلاَمُ وَرَحْمَةُ اللهِ وَبَرَكَاتُهُ`")
+# Menjawab Salam
+
+
+@register(outgoing=True, pattern="^.istigfar(?: |$)(.*)")
+async def perkenalan(event):
+    event.pattern_match.group(1)
+    await event.edit(f"`Heh Kamu Gaboleh Begitu...`")
+    sleep(2)
+    await event.edit("`اَسْتَغْفِرُاللهَ الْعَظِيْم`")
+# Istigfar
+
+
+@register(outgoing=True, pattern="^.perkenalan(?: |$)(.*)")
+async def perkenalan(event):
+    event.pattern_match.group(1)
+    await event.edit(f"`Hai Guys , Perkenalkan Nama Gw {DEFAULTUSER}`")
+    sleep(2)
+    await event.edit(f"`Gw Tinggal Di {WEATHER_DEFCITY}`")
+    sleep(2)
+    await event.edit("`Salam Kenal...`")
+    sleep(2)
+    await event.edit("`Udah Gitu Aja :v`")
+# Perkenalan
+
+
+CMD_HELP.update({
+    "gabut": "**Modules** - `Gabut`\
+    \n\n Cmd : `.l`\
+    \nUsage : Untuk Menjawab Salam\
+    \n\n Cmd : `.perkenalan`\
+    \nUsage : Memperkenalkan Diri\
+    \n\n Cmd : `.g`\
+    \nUsage : Member Goblok\
+    \n\n Cmd : `.p`\
+    \nUsage : Untuk Memberi Salam."
+})
