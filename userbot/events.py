104ddda69307b139af7f94a7628983c46ac7ea9b
ᴷᵞᵞ <89422473+muhammadrizky16@users.noreply.github.com>
2021-10-08 09:57:59 +0700

    Update events.py

--- userbot/events.py
+++ userbot/events.py
@@ -96,7 +96,7 @@ def register(**args):
                 if not disable_errors:
                     date = strftime("%Y-%m-%d %H:%M:%S", gmtime())
 
-                    text = "**Kyy-Project ERROR**\n"
+                    text = "**Kyy-Userbot ERROR**\n"
                     link = "Silahkan chat: @IDnyaKosong"
                     text += "Untuk melaporkan kesalahan"
                     text += f"tinggal teruskan pesan ini {link}.\n"
