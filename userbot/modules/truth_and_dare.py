6b383aedc7830f7f4de113502c5948687141ec77
ᴷᵞᵞ <89422473+muhammadrizky16@users.noreply.github.com>
2021-10-19 14:51:42 +0700

    Create truth_and_dare.py

---
+++ userbot/modules/truth_and_dare.py
@@ -0,0 +1,34 @@
+import requests
+
+from userbot import CMD_HELP
+from userbot.events import register
+from userbot.utils import edit_or_reply
+
+
+@register(outgoing=True, pattern="^.truth$")
+async def tede_truth(event):
+    try:
+        resp = requests.get("https://api-tede.herokuapp.com/api/truth").json()
+        results = resp["message"]
+        await edit_or_reply(event, f"**#Truth**\n\n`{results}`")
+    except Exception:
+        await edit_or_reply(event, "**Something went wrong LOL...**")
+
+
+@register(outgoing=True, pattern="^.dare$")
+async def tede_dare(event):
+    try:
+        resp = requests.get("https://api-tede.herokuapp.com/api/dare").json()
+        results = resp["message"]
+        await edit_or_reply(event, f"**#Dare**\n\n`{results}`")
+    except Exception:
+        await edit_or_reply(event, "**Something went wrong LOL...**")
+
+
+CMD_HELP.update({
+        "truthdare": f"**Plugin : **`truthdare`\
+        \n\n  •  **Syntax :** `.truth`\
+        \n  •  **Function : **Untuk tantangan.\
+        \n\n  •  **Syntax :** `.dare`\
+        \n  •  **Function : **Untuk kejujuran."
+})
