b062f0918c5e2d053a499be4fbb52b09a35a6622
ᴷᵞᵞ <89422473+muhammadrizky16@users.noreply.github.com>
2021-10-24 02:55:34 +0700

    clear

--- userbot/modules/war.py
+++ userbot/modules/war.py
@@ -115,66 +115,6 @@ async def typewriter(typew):
     )
 
 
-@register(outgoing=True, pattern=r"^\.dih(?: |$)(.*)")
-async def typewriter(typew):
-    typew.pattern_match.group(1)
-    await typew.edit(
-        "**DIHHH NAJISS ANAK HARAM LO GOBLOK, JANGAN BELAGU DIMARI KAGA KEREN LU KEK BGITU KONTOL!**"
-    )
-
-
-@register(outgoing=True, pattern=r"^\.gcs(?: |$)(.*)")
-async def typewriter(typew):
-    typew.pattern_match.group(1)
-    await typew.edit("**GC SAMPAH KAYA GINI, BUBARIN AJA GOBLOK!!**")
-
-
-@register(outgoing=True, pattern=r"^\.skb(?: |$)(.*)")
-async def typewriter(typew):
-    typew.pattern_match.group(1)
-    await typew.edit("**EMANG KITA KENAL? KAGA GOBLOK SOKAB BANGET LU GOBLOK**")
-
-
-@register(outgoing=True, pattern=r"^\.an(?: |$)(.*)")
-async def typewriter(typew):
-    typew.pattern_match.group(1)
-    await typew.edit(
-        "**LO KAN HASIL ZINAH HASIL HUB TERLARANG NAH LO DI BUANG DI TONGSAMPAH NAH MAK LO YG SKRG KASIAN SAMA LO MKNY DI PUNGUT UDH LO CACAT KAKI CACAT TANGAN CACAT MUKA CACAT SEMUAHNY PARAH BET SI KONTOL LO JUGA CACATAN PASTI NANAHAN JUGA,UDH MENDING LO BUNDIR DEH JADI BEBAN ORG DOANG BEGO NGERUGIN MASYARKAT BEGO BOCAH HINA BOCAH HARAM BOCAH AUTIS KEK LO MENDING MATI AJA**"
-    )
-
-
-@register(outgoing=True, pattern=r"^\.bk(?: |$)(.*)")
-async def typewriter(typew):
-    typew.pattern_match.group(1)
-    await typew.edit(
-        "**BUAT LO KONTOL NIH KALO UDAH HINA GAUSAH SOK SOK NGEHINA HINA GUA KONTOL, GUA TERLALU SUCI BUAT LU YANG HINA ITU ADUHHH. SINI GUA LUDAHIN DLU LU BIAR DIRI LU SUCI KARENA LU TAU LUDAH GUA ITU MULIA SEKALI**"
-    )
-
-
-@register(outgoing=True, pattern=r"^\.gj(?: |$)(.*)")
-async def typewriter(typew):
-    typew.pattern_match.group(1)
-    await typew.edit(
-        "**YA AMPUN LU NGOMONG APA? GA NYAMBUNG KONTOL KAYA KEHIDUPAN LU MAKANYA ORG ORG KAYA LU GABAKALN MAJU HIDUPNYA APA LAGI ORG ORG BAWAHAN KAYA LU.**"
-    )
-
-
-@register(outgoing=True, pattern=r"^\.gh(?: |$)(.*)")
-async def typewriter(typew):
-    typew.pattern_match.group(1)
-    await typew.edit(
-        "**DUH GINI NIH BOCAH YG LAHIR DI GUBUK BAMBU REOT + GAPUNYA HARGA DIRI, PADAHAL MAH DARI KECIL DIAJARIN SM EMAKNYA GABOLEH SONGONG SM MAJIKAN MASIH AJA SONGONG, MENDING LO URUSIN DULU GOBLOK KELUARGA LO YG PENYAKITAN ITU, MANA BAPA LO KAKINYA BOROK BEGITU AJG BERNANAH BAU AMIS IDIH GELI BET GELI GUA LIATNYA, NAH SEKALIAN TUH URUSIN JUGA ADE LO TUH, KALO BUKAN KARENA GUA MAH ADE LO UDAH MENINGGAL KENA TUMOR TOLOL MAKANYA LO KUDU SUJUD DEPAN GUA YAKAN,EMAK LO JUGA TUH JAGAIN UDAH BISU BEGITU YAKAN TAKUTNYA JATOH GABISA TREAK, MAKANYA NIH YA JANGAN KEBANYAKAN KONSUMSI SASA MICIN GOBLOK LIAT KAN EFEKNYA LO JADI KEK BOCAH AYAN BEGITU, SAMPE² LO BERANI GITU YAKAN NYENGGET JEMURAN ORANG SAMPE LO DIPUKULIN TRUS DI INJEK² SAMA WARGA SEKAMPUNGAN, GINI YA GUA KASIH TAU NIH SAMA LO NIH KALO UDA MISKIN KAGA USAH BELAGU SEGALA TOLOL, MIKIR LO MAKAN AJA SUSAH SAMPE NGEMIS² DI KOMPLEK PERUMAHAN GUA SAMPE DI USIR SAMA SATPAM KOMPLEK GUA, BERAS AJA LO BOLEH DIBAGI SAMA EMAK GUA YAKAN LAUK PAUK IKAN, AYAM, DAGING SEGALA RUPA AJA LO BOLEH NYOLONG DARI PASAR BOCAH KAYA LO MAH GIZINYA KURANG DONGO SABAN HARI MAKAN INDOMI 1 PAKE TELOR DOANG ITU JUGA JOINAN SM KELUARGA LU, KARENA APA?, YA KARENA LO MISKIN GA MAMPU BELI MAKANAN YG BERGIZI, DIKASIH KUAH SAYUR KANGKUNG JUGA MAO TOLOL ITU JUGA UDAH BERSYUKUR BISA MAKAN MAKANAN SELAEN MI INSTAN YAKAN SECARA LO GABISA GITU KEK GUA YAKAN MAKAN APA YG GUA MAO LAH ELO MAKAN MAKANAN TONG SAMPAH JUGA UDAH ALHAMDULILLAH BANGET AJG**"
-    )
-
-
-@register(outgoing=True, pattern=r"^\.lol(?: |$)(.*)")
-async def typewriter(typew):
-    typew.pattern_match.group(1)
-    await typew.edit(
-        "**TALAL TOLOL TALAL TOLOL KEK ORG DONGO, NIH YA DENGERIN MAKANYA BESOK² JANGAN NGELEM MULU OTAK LO, JADI LEMBEK DAH TUH PALA, MANA SABAN HARI MAKE SAMPO SACHET YG SEREBUAN GIMANA GA KETOMBEAN TU PALA, YA JELAS LAH BEDA SAMA GUA YA GUA SI PAKE SAMPO BOTOLAN GITU YAKAN, MANA SABUN BETAK DI RUMAH ORANG BEKAS COLI JUGA DIPAKE SAMA KELUARGA LO BUAT MANDI YA KARENA APA?, KARNA KELUARGA LO TUH HINA TOLOL BAHKAN LEBIH HINA DARIPADA HEWAN, LO KAGA PANTES MAEN TELE SUMPAH MENDING LO SEKOLAH DULU DAH YG BENER DI BIAYAIN AMA PEMERINTAH AJA GATAU DIRI KONTOL, LO PIKIR BAPA EMAK LO MAMPU GITU BIAYAIN LO SEKOLAH?, KAGA GOBLOK GUA MAH TAU BET TAU LO TU CUMA BABU DI TONGKRONGAN KAN, PALING² JUGA YA LO NONGKRONG BAWA GOCENG YA MANA ADA YG MAO TEMENAN AM ORG SUSAH JELEK HINA DEKIL KAYA LO TOLOL, APALAGI CEWE, MANA ADA CEWE YG MAO AM ORG KAYA LO, PALINGAN JUGA DIAJAK JALANNYA KE RAWA² YAKAN MANA MAO NGEWE TAPI GAMAO MODAL YA JADI NGEWENYA DI SEMAK² RAWA DAH TUH MANA DIGIGITIN SEMUT RANGRANG SAMPE PADA BURIK TUH KAKI CEWE LO, BUKANNYA MIKIR GITU YAKAN MALAH PLANGA PLONGO TAWA TAWA KEK BOCAH BLOON**"
-    )
-
-
 CMD_HELP.update(
     {
         "war": "**Plugin : **`war`\
@@ -208,20 +148,6 @@ CMD_HELP.update(
         \n  •  **Function : **Ngeledek bapaknya si jamet\
         \n\n  •  **Syntax :** `.cuih`\
         \n  •  **Function : **Ngeludahin keluarganya satu satu wkwk\
-        \n\n  •  **Syntax :** `.dih`\
-        \n  •  **Function : **Ngeledek anak haram\
-        \n\n  •  **Syntax :** `.gcs`\
-        \n  •  **Function : **Ngeledek gc sampah\
-        \n\n  •  **Syntax :** `.an`\
-        \n  •  **Function : **Ngatain anak pungut\
-        \n\n  •  **Syntax :** `.bk`\
-        \n  •  **Function : **Ngatain bocah hina\
-        \n\n  •  **Syntax :** `.gj`\
-        \n  •  **Function : **Ngatain bocah gajelas\
-        \n\n  •  **Syntax :** `.gh`\
-        \n  •  **Function : **liat jee sendiri\
-        \n\n  •  **Syntax :** `.lol`\
-        \n  •  **Function : **ngatain bocah tolol\
         \n\n**Klo mau Req, kosa kata dari lu Hubungi @IDnyaKosong**\
     "
     }
