d6ff1db7ad451a7237d388aaf58e206db425b3c6
ᴷᵞᵞ <89422473+muhammadrizky16@users.noreply.github.com>
2021-10-23 13:05:56 +0700

    Update war.py

--- userbot/modules/war.py
+++ userbot/modules/war.py
@@ -29,28 +29,6 @@ async def typewriter(typew):
     await typew.edit("**LEMBEK NGENTOT🔥**")
 
 
-@register(outgoing=True, pattern=r"^\.virtual(?: |$)(.*)")
-async def typewriter(typew):
-    typew.pattern_match.group(1)
-    await typew.edit("**OOOO**")
-    sleep(1.5)
-    await typew.edit("**INI YANG VIRTUAL**")
-    sleep(1.5)
-    await typew.edit("**YANG KATANYA SAYANG BANGET**")
-    sleep(1.5)
-    await typew.edit("**TAPI TETEP AJA DI TINGGAL**")
-    sleep(1.5)
-    await typew.edit("**NI INGET**")
-    sleep(1.5)
-    await typew.edit("**TANGANNYA AJA GA BISA DI PEGANG**")
-    sleep(1.5)
-    await typew.edit("**APALAGI OMONGANNYA**")
-    sleep(1.5)
-    await typew.edit("**BHAHAHAHA**")
-    sleep(1.5)
-    await typew.edit("**KASIAN MANA MASIH MUDA**")
-
-
 @register(outgoing=True, pattern=r"^\.pp(?: |$)(.*)")
 async def typewriter(typew):
     typew.pattern_match.group(1)
@@ -202,8 +180,6 @@ CMD_HELP.update(
         "war": "**Plugin : **`war`\
         \n\n  •  **Syntax :** `.jamet`\
         \n  •  **Function : **Menghina Jamet telegram\
-        \n\n  •  **Syntax :** `.virtual`\
-        \n  •  **Function : **Menghina orang yg virtual\
         \n\n  •  **Syntax :** `.pp`\
         \n  •  **Function : **Menghina Jamet telegram yang ga pake foto profil\
         \n\n  •  **Syntax :** `.dp`\
