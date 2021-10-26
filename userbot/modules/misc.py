6595c108db6320a8b971e5a31254574df93e0f3a
ᴷᵞᵞ <89422473+muhammadrizky16@users.noreply.github.com>
2021-10-27 04:17:21 +0700

    ok

--- userbot/modules/misc.py
+++ userbot/modules/misc.py
@@ -114,7 +114,7 @@ async def repo_is_here(wannasee):
         "卍━━━━━━━━━━━━━━━━━━━━━━卍\n"
         "➣ **Repo Userbot :** [ɢɪᴛʜᴜʙ](https://github.com/muhammadrizky16/Kyy-Userbot)\n"
         "➣ **Owner Bot :** [ҡʏʏ](t.me/IDnyaKosong)\n"
-        "卍━━━━━━━━━━━━━━━━━━━━━━━卍\n"
+        "卍━━━━━━━━━━━━━━━━━━━━━━卍\n"
         "➣ **Instagram :** [ɪɴsᴛᴀɢʀᴀᴍ](instagram.com/rizkyhamdanii16_)\n"
         "➣ **Support :** [ᴄʜᴀɴɴᴇʟ](https://t.me/ahhsudahlahhh)\n"
         "卍━━━━━━━━━━━━━━━━━━━━━━卍\n"
