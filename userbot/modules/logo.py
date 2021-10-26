11c16d3aafa93f29d92e299bc83ad1b408175996
ᴷᵞᵞ <89422473+muhammadrizky16@users.noreply.github.com>
2021-10-26 14:50:35 +0700

    Update logo.py

--- userbot/modules/logo.py
+++ userbot/modules/logo.py
@@ -18,7 +18,7 @@ async def _(event):
         await event.edit("`Give a name too!`")
     else:
         await event.edit("`Processing`")
-    chat = "@tdtapibot"
+    chat = "@Akiramanagerbot"
     async with event.client.conversation(chat) as conv:
         try:
             msg = await conv.send_message(f"/logo {text}")
@@ -28,7 +28,7 @@ async def _(event):
             await event.client.send_read_acknowledge(conv.chat_id)
         except YouBlockedUserError:
             await event.edit(
-                "**Error: Mohon Buka Blokir** @tdtapibot **Dan Coba Lagi!**"
+                "**Error: Mohon Buka Blokir** @Akiramanagerbot **Dan Coba Lagi!**"
             )
             return
         await asyncio.sleep(0.5)
