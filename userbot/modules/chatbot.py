76c3968f5f5a3f998706bb8ded15ca34b614e861
ᴷᵞᵞ <89422473+muhammadrizky16@users.noreply.github.com>
2021-11-01 06:57:43 +0700

    Update chatbot.py

--- userbot/modules/chatbot.py
+++ userbot/modules/chatbot.py
@@ -8,12 +8,10 @@ from telethon.tl.types import User
 
 from userbot import CMD_HELP, LOGS, bot
 from userbot.events import register
-from userbot.utils import edit_or_reply
+from userbot.modules.sql_helper.tede_chatbot_sql import is_tede, rem_tede, set_tede
 
 translator = Translator()
 LANGUAGE = "id"
-
-aktifnya_chat_bot = []
 
 url = "https://api-tede.herokuapp.com/api/chatbot?message={message}"
 
@@ -30,32 +28,37 @@ async def ngapain_rep(message):
         LOGS.info(str(e))
 
 
-async def chat_bot_toggle(db, event):
+async def chat_bot_toggle(event):
     status = event.pattern_match.group(1).lower()
     chat_id = event.chat_id
     if status == "on":
-        if chat_id not in db:
-            db.append(chat_id)
-            return await edit_or_reply(event, "ChatBot Diaktifkan!")
-        await edit_or_reply(event, "ChatBot Sudah Diaktifkan.")
+        if not is_tede(chat_id):
+            set_tede(chat_id)
+            return await event.edit("ChatBot Diaktifkan!")
+        await event.edit("ChatBot Sudah Diaktifkan.")
     elif status == "off":
-        if chat_id in db:
-            db.remove(chat_id)
-            return await edit_or_reply(event, "ChatBot Dinonaktifkan!")
-        await edit_or_reply(event, "ChatBot Sudah Dinonaktifkan.")
+        if is_tede(chat_id):
+            rem_tede(chat_id)
+            return await event.edit("ChatBot Dinonaktifkan!")
+        await event.edit("ChatBot Sudah Dinonaktifkan.")
     else:
-        await edit_or_reply(event, "**Usage:** `.chatbot` <on/off>")
+        await event.edit("**Usage:** `.chatbot` <on/off>")
 
 
 @register(outgoing=True, pattern=r"^\.chatbot(?: |$)(.*)")
 async def on_apa_off(event):
-    await chat_bot_toggle(aktifnya_chat_bot, event)
+    await chat_bot_toggle(event)
 
 
-@bot.on(events.NewMessage(incoming=True))
+@bot.on(
+    events.NewMessage(
+        incoming=True,
+        func=lambda e: (e.mentioned),
+    ),
+)
 async def tede_chatbot(event):
     sender = await event.get_sender()
-    if event.chat_id not in aktifnya_chat_bot:
+    if not is_tede(event.chat_id):
         return
     if not isinstance(sender, User):
         return
@@ -64,13 +67,15 @@ async def tede_chatbot(event):
         tr = translator.translate(rep, LANGUAGE)
         if tr:
             await event.reply(tr.text)
+        else:
+            await event.reply(rep)
 
 
 CMD_HELP.update(
     {
         "chatbot": "**Plugin : **`chatbot`\
       \n\n  •  **Syntax :** `.chatbot` <on/off>\
-      \n  •  **Function :** Ya chatbot\
+      \n  •  **Function :** Untuk membalas chat dengan chatbot AI.\
       "
     }
 )
