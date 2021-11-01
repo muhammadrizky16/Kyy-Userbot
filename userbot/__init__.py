6726994e6cb1ee23597b9ecc5f6ea5c2e94f8b50
ᴷᵞᵞ <89422473+muhammadrizky16@users.noreply.github.com>
2021-11-02 01:36:20 +0700

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -361,7 +361,7 @@ with bot:
 
 
 async def check_alive():
-    await bot.send_file(BOTLOG_CHATID, caption=f"**Userbot has been deployed✨**\n━━━━━━━━━━━━━━━━━━━\n**Bot Of:** {DEFAULTUSER}\n━━━━━━━━━━━━━━━━━━━\n**Support:** [ᴄʜᴀɴɴᴇʟ](https://t.me/ahhsudahlahhh)\n━━━━━━━━━━━━━━━━━━━")
+    await bot.send_file(BOTLOG_CHATID, caption=f"**Userbot has been deployed✨**\n━━━━━━━━━━━━━━━━━━━\n**Bot Of:** {DEFAULTUSER}\n━━━━━━━━━━━━━━━━━━━\n**Support:** @ahhsudahlahhh \n━━━━━━━━━━━━━━━━━━━")
     return
 
 
