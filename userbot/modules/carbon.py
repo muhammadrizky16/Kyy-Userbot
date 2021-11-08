e96e57d516d9ba72e8ea79b7385e6e5684406334
ᴷᵞᵞ <89422473+muhammadrizky16@users.noreply.github.com>
2021-11-08 09:43:47 +0700

    Update carbon.py

--- userbot/modules/carbon.py
+++ userbot/modules/carbon.py
@@ -80,7 +80,7 @@ async def carbon_api(e):
         e.chat_id,
         file,
         caption="Made Using [Carbon](https://carbon.now.sh/about/),\
-        \na Join Grub [Rose](https://t.me/Rose_Userbot)",
+        \na Join Channel [Kyy](https://t.me/ahhsudahlahhh)",
         force_document=True,
         reply_to=e.message.reply_to_msg_id,
     )
@@ -146,7 +146,7 @@ async def carbon_api(e):
         e.chat_id,
         file,
         caption="Made Using [Carbon](https://carbon.now.sh/about/),\
-        \na Join Grub [Rose](https://t.me/Rose_Userbot)",
+        \na Join Channel [Kyy](https://t.me/ahhsudahlahhh)",
         force_document=True,
         reply_to=e.message.reply_to_msg_id,
     )
@@ -212,7 +212,7 @@ async def carbon_api(e):
         e.chat_id,
         file,
         caption="Made Using [Carbon](https://carbon.now.sh/about/),\
-        \na Join Grub [Rose](https://t.me/Rose_Userbot)",
+        \na Join Channel [Kyy](https://t.me/ahhsudahlahhh)",
         force_document=True,
         reply_to=e.message.reply_to_msg_id,
     )
@@ -278,7 +278,7 @@ async def carbon_api(e):
         e.chat_id,
         file,
         caption="Made Using [Carbon](https://carbon.now.sh/about/),\
-        \na Join Grub [Rose](https:/t.me/Rose_Userbot)",
+        \na Join Channel [Kyy](https:/t.me/ahhsudahlahhh)",
         force_document=True,
         reply_to=e.message.reply_to_msg_id,
     )
@@ -344,7 +344,7 @@ async def carbon_api(e):
         e.chat_id,
         file,
         caption="Made Using [Carbon](https://carbon.now.sh/about/),\
-        \na Join Grub [Rose](https://t.me/Rose_Userbot)",
+        \na Join Channel [Kyy](https://t.me/ahhsudahlahhh)",
         force_document=True,
         reply_to=e.message.reply_to_msg_id,
     )
