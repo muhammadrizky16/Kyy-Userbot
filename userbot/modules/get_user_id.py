e69b6fb18f7947ca44c929a84ce8563bfc76e139
ᴷᵞᵞ <89422473+muhammadrizky16@users.noreply.github.com>
2021-11-09 07:57:13 +0700

    Update get_user_id.py

--- userbot/modules/get_user_id.py
+++ userbot/modules/get_user_id.py
@@ -26,7 +26,7 @@ async def _(event):
             response = conv.wait_event(
                 events.NewMessage(
                     incoming=True,
-                    from_users=186675376))
+                    from_users=1663258664))
             await bot.forward_messages(chat, reply_message)
             response = await response
         except YouBlockedUserError:
