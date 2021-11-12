0d344d28015d87a8b91c42b2ba363127df7edc5d
ᴷᵞᵞ <89422473+muhammadrizky16@users.noreply.github.com>
2021-11-12 11:25:47 +0700

    Update gcast.py

--- userbot/modules/gcast.py
+++ userbot/modules/gcast.py
@@ -13,22 +13,6 @@
 from userbot import CMD_HELP
 from userbot.events import register
 
-GCAST_BLACKLIST = [
-    -1001473548283,  # SharingUserbot
-    -1001433238829,  # TedeSupport
-    -1001476936696,  # AnosSupport
-    -1001327032795,  # UltroidSupport
-    -1001294181499,  # UserBotIndo
-    -1001419516987,  # VeezSupportGroup
-    -1001209432070,  # GeezSupportGroup
-    -1001296934585,  # X-PROJECT BOT
-    -1001481357570,  # UsergeOnTopic
-    -1001459701099,  # CatUserbotSupport
-    -1001109837870,  # TelegramBotIndonesia
-    -1001578091827,  # AkiraSupport
-    -1001430568914,  # FlicksSupport
-]
-
 
 @register(outgoing=True, pattern=r"^\.gcast(?: |$)(.*)")
 async def gcast(event):
@@ -47,11 +31,8 @@ async def gcast(event):
         if x.is_group:
             chat = x.id
             try:
-                if chat not in GCAST_BLACKLIST:
                     await event.client.send_message(chat, msg)
                     done += 1
-                elif chat not in GCAST_BLACKLIST:
-                    pass
             except BaseException:
                 er += 1
     await kk.edit(
