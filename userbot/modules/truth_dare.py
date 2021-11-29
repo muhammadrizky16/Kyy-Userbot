204fd4264b15dafb0f9e322ba4a8fee8b864f21b
ᴷʸʸ <89422473+muhammadrizky16@users.noreply.github.com>
2021-11-29 19:02:06 +0700

    Update truth_dare.py

--- userbot/modules/truth_dare.py
+++ userbot/modules/truth_dare.py
@@ -40,13 +40,32 @@ async def _(event):
         await event.edit(f"**Pesan dare**\n\n{response.message.message}")
 
 
+@register(outgoing=True, pattern=r"^\.spill(?: |$)(.*)")
+async def _(event):
+    await event.edit("Mengirim pesan spill...")
+    async with bot.conversation("@Spillgame_bot") as conv:
+        try:
+            response = conv.wait_event(
+                events.NewMessage(incoming=True, from_users=1361222893)
+            )
+            await conv.send_message("/spill")
+            response = await response
+            await bot.send_read_acknowledge(conv.chat_id)
+        except YouBlockedUserError:
+            await event.edit("Harap unblock `@Spillgame_bot` dan coba lagi")
+            return
+        await event.edit(f"**Pesan spill**\n\n{response.message.message}")
+
+
 CMD_HELP.update(
     {
         "truth_dare": "** Plugin :** truth_dare\
         \n\n  •  Perintah : `.truth`\
-        \n  •  Function : Untuk mengirim pesan truth secara random\
+        \n  •  Function : Untuk mengirim pesan truth\
         \n\n  •  Perintah : `.dare`\
-        \n  •  Function : Untuk mengirim pesan dare secara random\
+        \n  •  Function : Untuk mengirim pesan dare\
+        \n\n  •  Perintah : `.spill`\
+        \n  •  Function : Untuk Pertanyaan\
     "
     }
 )
