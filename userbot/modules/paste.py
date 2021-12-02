67ef0d2c8a9aa9983a898fe125d962c9acd43961
ᴷʸʸ <89422473+muhammadrizky16@users.noreply.github.com>
2021-12-02 18:34:37 +0700

    Update paste.py

--- userbot/modules/paste.py
+++ userbot/modules/paste.py
@@ -7,12 +7,13 @@
 
 import os
 
+from userbot import register
 from userbot import CMD_HELP, TEMP_DOWNLOAD_DIRECTORY
 from userbot.utils import edit_delete, edit_or_reply
 from userbot.utils.pastebin import PasteBin
 
 
-@man_cmd(pattern="paste(?: (-d|-n|-h|-k)|$)?(?: ([\\s\\S]+)|$)")
+@register(pattern="paste(?: (-d|-n|-h|-k)|$)?(?: ([\\s\\S]+)|$)")
 async def paste(pstl):
     """For .paste command, pastes the text directly to a pastebin."""
     service = pstl.pattern_match.group(1)
