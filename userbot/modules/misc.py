13d515928ab17a6292ef12679614ffc98709233d
ᴷʸʸ <89422473+muhammadrizky16@users.noreply.github.com>
2021-12-02 23:28:34 +0700

    Update misc.py

--- userbot/modules/misc.py
+++ userbot/modules/misc.py
@@ -115,8 +115,8 @@ async def repo_is_here(wannasee):
         "➣ **Repo Userbot :** [ɢɪᴛʜᴜʙ](https://github.com/muhammadrizky16/Kyy-Userbot)\n"
         "➣ **Owner Bot       :** [ҡʏʏ](t.me/IDnyaKosong)\n"
         "卍━━━━━━━━━━━━━━━━━━━━━━卍\n"
-        "➣ **Instagram       :** [ɪɴsᴛᴀɢʀᴀᴍ](instagram.com/rizkyhamdanii16_)\n"
-        "➣ **Support           :** [ᴄʜᴀɴɴᴇʟ](https://t.me/ahhsudahlahhh)\n"
+        "➣ **Support       :** [sᴜᴘᴘᴏʀᴛ](https://t.me/NastySupportt)\n"
+        "➣ **Channel           :** [ᴄʜᴀɴɴᴇʟ](https://t.me/ahhsudahlahhh)\n"
         "卍━━━━━━━━━━━━━━━━━━━━━━卍\n"
     )
 
