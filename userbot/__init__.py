f08ec7571bbdb1ff993a5051911aa24bbe8bc820
ᴷʸʸ <89422473+muhammadrizky16@users.noreply.github.com>
2022-01-25 23:42:39 +0700

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -193,7 +193,7 @@ YOUTUBE_API_KEY = os.environ.get(
     "YOUTUBE_API_KEY") or "AIzaSyACwFrVv-mlhICIOCvDQgaabo6RIoaK8Dg"
 
 # Untuk Perintah .kyyalive
-KYY_TEKS_KUSTOM = os.environ.get("KYY_TEKS_KUSTOM", "I'am Using Kyy-Userbot ✨")
+KYY_TEKS_KUSTOM = os.environ.get("KYY_TEKS_KUSTOM", "I'am Using Kyy-Userbot✨")
 
 # Untuk Mengubah Pesan Welcome
 START_WELCOME = os.environ.get("START_WELCOME", None)
