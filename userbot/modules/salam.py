fd4c28497d802027d8f0973a828bd6db7efaa679
ᴷʸʸ <89422473+muhammadrizky16@users.noreply.github.com>
2022-02-08 11:51:49 +0700

    heum

--- userbot/modules/salam.py
+++ userbot/modules/salam.py
@@ -1,31 +1,31 @@
 from platform import uname
-from userbot import ALIVE_NAME, CMD_HELP
-from userbot.events import register
+from userbot import ALIVE_NAME, CMD_HELP, CMD_HANDLER as cmd
+from userbot.utils import kyy_cmd
 
 # ================= CONSTANT =================
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
 # ============================================
 
 
-@register(outgoing=True, pattern='^P(?: |$)(.*)')
+@kyy_cmd(pattern='^P(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
     await typew.edit("𝐀𝐬𝐬𝐚𝐥𝐚𝐦𝐮'𝐚𝐥𝐚𝐢𝐤𝐮𝐦...")
 
 
-@register(outgoing=True, pattern='^.atg(?: |$)(.*)')
+@kyy_cmd(pattern='^.atg(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
     await typew.edit("𝐀𝐒𝐓𝐀𝐆𝐇𝐅𝐈𝐑𝐔𝐋𝐋𝐀𝐇....SAYANG!!!!")
 
 
-@register(outgoing=True, pattern='^L(?: |$)(.*)')
+@kyy_cmd(pattern='^L(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
     await typew.edit("𝐖𝐚'𝐚𝐥𝐚𝐢𝐤𝐮𝐦𝐬𝐚𝐥𝐚𝐦...")
 
 
-@register(outgoing=True, pattern='^.ast(?: |$)(.*)')
+@kyy_cmd(pattern='^.ast(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
     await typew.edit("𝐀𝐒𝐓𝐀𝐆𝐇𝐅𝐈𝐑𝐔𝐋𝐋𝐀𝐇......")
@@ -42,8 +42,8 @@ CMD_HELP.update({
 
 CMD_HELP.update({
     "salam2":
-    ".atg\
+    f"{cmd}atg\
 \nUsage: Istighfar 1.\
-\n\n.ast\
+\n\n{cmd}ast\
 \nUsage: Istighfaf 2."
 })
