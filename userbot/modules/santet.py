e375bbda360f46f0f2813dfb70474a87a581e75c
ᴷʸʸ <89422473+muhammadrizky16@users.noreply.github.com>
2022-02-08 12:55:55 +0700

    hehe

--- userbot/modules/santet.py
+++ userbot/modules/santet.py
@@ -3,7 +3,7 @@ from userbot import CMD_HELP, CMD_HANDLER as cmd
 from userbot.utils import kyy_cmd
 
 
-@kyy_cmd(pattern="santet(?: |$)(.*)')
+@kyy_cmd(pattern='santet(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
     await typew.edit("`Anda Telah Mengaktifkan Perintah Santet Online ツ`")
