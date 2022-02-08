ee31b873d4590311ae4b8dd63e88fafe78e8a41b
ᴷʸʸ <89422473+muhammadrizky16@users.noreply.github.com>
2022-02-08 11:23:36 +0700

    INI LAGI ANJ

--- userbot/modules/inviteall.py
+++ userbot/modules/inviteall.py
@@ -11,6 +11,7 @@ from telethon.tl.functions.channels import GetFullChannelRequest
 from telethon.tl.functions.messages import GetFullChatRequest
 
 from userbot import CMD_HELP, CMD_HANDLER as cmd
+from userbot.events import register
 from userbot.utils import kyy_cmd
 
 
