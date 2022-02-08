a486fff2acd1bd871cfd0a976eb862e0d501bad6
Fariz <84852749+fjgaming212@users.noreply.github.com>
2022-02-08 07:54:38 +0700

    Kyy-Userbot: Add custom Handler

---
+++ userbot/utils/decorator.py
@@ -0,0 +1,122 @@
+# Credits: @mrconfused
+# Recode by @mrismanaziz
+# FROM Man-Userbot <https://github.com/mrismanaziz/Man-Userbot>
+# t.me/SharingUserbot & t.me/Lunatic0de
+
+import inspect
+import re
+from pathlib import Path
+
+from telethon import events
+
+from userbot import (
+    BL_CHAT,
+    CMD_HANDLER,
+    CMD_LIST,
+    LOAD_PLUG,
+    SUDO_HANDLER,
+    SUDO_USERS,
+    bot,
+    tgbot,
+)
+
+
+def kyy_cmd(
+    pattern: str = None,
+    allow_sudo: bool = True,
+    disable_edited: bool = False,
+    forword=False,
+    command: str = None,
+    **args,
+):
+    args["func"] = lambda e: e.via_bot_id is None
+    stack = inspect.stack()
+    previous_stack_frame = stack[1]
+    file_test = Path(previous_stack_frame.filename)
+    file_test = file_test.stem.replace(".py", "")
+
+    if "disable_edited" in args:
+        del args["disable_edited"]
+
+    args["blacklist_chats"] = True
+    black_list_chats = list(BL_CHAT)
+    if len(black_list_chats) > 0:
+        args["chats"] = black_list_chats
+
+    if pattern is not None:
+        global kyy_reg
+        global sudo_reg
+        if (
+            pattern.startswith(r"\#")
+            or not pattern.startswith(r"\#")
+            and pattern.startswith(r"^")
+        ):
+            kyy_reg = sudo_reg = re.compile(pattern)
+        else:
+            kyy_ = "\\" + CMD_HANDLER
+            sudo_ = "\\" + SUDO_HANDLER
+            kyy_reg = re.compile(kyy_ + pattern)
+            sudo_reg = re.compile(sudo_ + pattern)
+            if command is not None:
+                cmd1 = kyy_ + command
+                cmd2 = sudo_ + command
+            else:
+                cmd1 = (
+                    (kyy_ +
+                     pattern).replace(
+                        "$",
+                        "").replace(
+                        "\\",
+                        "").replace(
+                        "^",
+                        ""))
+                cmd2 = (
+                    (sudo_ + pattern)
+                    .replace("$", "")
+                    .replace("\\", "")
+                    .replace("^", "")
+                )
+            try:
+                CMD_LIST[file_test].append(cmd1)
+            except BaseException:
+                CMD_LIST.update({file_test: [cmd1]})
+
+    def decorator(func):
+        if not disable_edited:
+            bot.add_event_handler(
+                func, events.MessageEdited(
+                    **args, outgoing=True, pattern=kyy_reg))
+        bot.add_event_handler(
+            func, events.NewMessage(**args, outgoing=True, pattern=kyy_reg)
+        )
+        if allow_sudo:
+            if not disable_edited:
+                bot.add_event_handler(
+                    func,
+                    events.MessageEdited(
+                        **args, from_users=list(SUDO_USERS), pattern=sudo_reg
+                    ),
+                )
+            bot.add_event_handler(
+                func,
+                events.NewMessage(
+                    **args, from_users=list(SUDO_USERS), pattern=sudo_reg
+                ),
+            )
+        try:
+            LOAD_PLUG[file_test].append(func)
+        except Exception:
+            LOAD_PLUG.update({file_test: [func]})
+        return func
+
+    return decorator
+
+
+def kyy_handler(
+    **args,
+):
+    def decorator(func):
+        bot.add_event_handler(func, events.NewMessage(**args, incoming=True))
+        return func
+
+    return decorator
