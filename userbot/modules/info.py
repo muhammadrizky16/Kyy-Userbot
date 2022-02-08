87e6530473893d55344aed1b4058e277003c0daf
ᴷʸʸ <89422473+muhammadrizky16@users.noreply.github.com>
2022-02-08 10:46:56 +0700

    try

--- userbot/modules/info.py
+++ userbot/modules/info.py
@@ -13,11 +13,11 @@ from telethon.tl.functions.photos import GetUserPhotosRequest
 from telethon.tl.functions.users import GetFullUserRequest
 from telethon.tl.types import MessageEntityMentionName
 from telethon.utils import get_input_location
-from userbot import CMD_HELP, TEMP_DOWNLOAD_DIRECTORY, CMD_HANDLER as cmd
-from userbot.utils import kyy_cmd
+from userbot import CMD_HELP, TEMP_DOWNLOAD_DIRECTORY
+from userbot.events import register
 
 
-@kyy_cmd(pattern=".info(?: |$)(.*)")
+@register(pattern=".info(?: |$)(.*)", outgoing=True)
 async def who(event):
 
     await event.edit(
@@ -147,6 +147,6 @@ async def fetch_info(replied_user, event):
 
 CMD_HELP.update({
     "info":
-    f">`{cmd}info <username> Atau Balas Ke Pesan Pengguna Ketik .info`"
+    ">`.info <username> Atau Balas Ke Pesan Pengguna Ketik .info`"
     "\nUsage: Mendapatkan Informasi Pengguna."
 })
