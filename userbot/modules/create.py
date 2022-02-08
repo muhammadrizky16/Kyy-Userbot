6be3bc7abaaf5da58c2e00e5a0ce6e313a3047e6
ᴷʸʸ <89422473+muhammadrizky16@users.noreply.github.com>
2022-02-08 10:09:55 +0700

    cpk

--- userbot/modules/create.py
+++ userbot/modules/create.py
@@ -1,7 +1,7 @@
 # lorduserbot
 from telethon.tl import functions
 from userbot.utils import kyy_cmd
-from userbot import CMD_HELP
+from userbot import CMD_HELP, CMD_HANDLER as cmd
 
 
 @kyy_cmd(pattern="^.buat (gb|g|c)(?: |$)(.*)")
@@ -42,14 +42,15 @@ async def telegraphs(grop):
             except Exception as e:  # pylint:disable=C0103,W0703
                 await grop.edit(str(e))
 
-CMD_HELP.update({
-    f"membuat": "\
-Membuat\
-\nUsage: Untuk membuat Channel, Grup dan Grup bersama Bot.\
-\n\n`{cmd}buat g` <nama grup>\
-\nUsage: Membuat grup mu.\
-\n\n`{cmd}buat gb` <nama grup>\
-\nUsage: Membuat Grup bersama bot.\
-\n\n`{cmd}buat c` <nama channel>\
-\nUsage: Membuat sebuah Channel.\
-"})
+CMD_HELP.update(
+    {
+        "membuat": f"**Plugin : **`membuat`\
+        \n\n  •  **Syntax :** `{cmd}buat g` <nama grup>\
+        \n  •  **Function : **Membuat grup telegram.\
+        \n\n  •  **Syntax :** `{cmd}buat gb` <nama grup>\
+        \n  •  **Function : **Membuat Grup bersama bot.\
+        \n\n  •  **Syntax :** `{cmd}buat c` <nama channel>\
+        \n  •  **Function : **Membuat sebuah Channel.\
+    "
+    }
+)
