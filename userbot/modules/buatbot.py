732e5ec53a9d076437b0a901b92951e08f63dfa7
ᴷʸʸ <89422473+muhammadrizky16@users.noreply.github.com>
2022-02-08 09:44:05 +0700

    add handler

--- userbot/modules/buatbot.py
+++ userbot/modules/buatbot.py
@@ -1,17 +1,16 @@
 # nyenyenyenye
 # FROM skyzu-userbot <https://github.com/Skyzu/skyzu-userbot>
-# port by koala🐨/@manusiarakitann
 
 from telethon.errors.rpcerrorlist import YouBlockedUserError
 from telethon.tl.functions.contacts import UnblockRequest
 
-from userbot import CMD_HELP
-from userbot.events import register
+from userbot import CMD_HELP, CMD_HANDLER as cmd
+from userbot.utils import kyy_cmd
 
 chat = "@BotFather"
 
 
-@register(outgoing=True, pattern="^.botbaru ?(.*)")
+@kyy_cmd(pattern="^.botbaru ?(.*)")
 async def _(event):
     if event.fwd_from:
         return
@@ -46,7 +45,7 @@ async def _(event):
 
 CMD_HELP.update(
     {
-        "botfather": ".botbaru\
+        "botfather": f"{cmd}botbaru\
     \nUntuk Membuat Bot Dari Botfather, .botbaru  < bot_name > <bot_username >  ."
     }
 )
