a81cf78602157e3d989e0e238c754f794be0420b
ᴷʸʸ <89422473+muhammadrizky16@users.noreply.github.com>
2022-02-09 07:31:56 +0700

    last ya

--- userbot/modules/www.py
+++ userbot/modules/www.py
@@ -165,12 +165,12 @@ async def pingme(pong):
     await pong.edit("**✣✣**")
     await pong.edit("**✣✣✣**")
     end = datetime.now()
-    (end - start).microseconds / 1000
+    duration = (end - start).microseconds / 1000
     await pong.edit(
         f"**Pɪɴɢᴇʀ :** "
         f"`%sms` \n"
         f"**Uᴘᴛɪᴍᴇ :** "
-        f"`{uptime}` \n"
+        f"`{uptime}` \n" & (duration)
     )
 
 
