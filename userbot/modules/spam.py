2670ade6161c080f861b59abcaf1895471d009be
ᴷʸʸ <89422473+muhammadrizky16@users.noreply.github.com>
2022-02-10 06:14:34 +0700

    Update spam.py

--- userbot/modules/spam.py
+++ userbot/modules/spam.py
@@ -38,13 +38,14 @@ async def tmeme(e):
 
 @kyy_cmd(pattern="spam(.*)")
 async def spammer(e):
-    counter = int(e.pattern_match.group(1).split(' ', 1)[0])
-    spam_message = str(e.pattern_match.group(1).split(' ', 1)[1])
+    counter = int(e.pattern_match.group(1).split(" ", 1)[0])
+    spam_message = str(e.pattern_match.group(1).split(" ", 1)[1])
     await e.delete()
     await asyncio.wait([e.respond(spam_message) for i in range(counter)])
     if BOTLOG:
-        await e.client.send_message(BOTLOG_CHATID, "#SPAM\n"
-                                    "Spam was executed successfully")
+        await e.client.send_message(
+            BOTLOG_CHATID, "#SPAM\n" "Spam was executed successfully"
+        )
 
 
 @kyy_cmd(pattern="picspam")
