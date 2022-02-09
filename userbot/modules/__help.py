0744b5d5ec1333b857425201749890ddcc80bcbf
CoeF <ubayyubaid@gmail.com>
2022-02-09 10:38:43 +0700

    try

--- userbot/modules/__help.py
+++ userbot/modules/__help.py
@@ -2,13 +2,14 @@ import logging
 
 from userbot import BOT_USERNAME
 from userbot.events import register
+from userbot.utils import kyy_cmd
 
 logging.basicConfig(
     format="[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s",
     level=logging.WARNING)
 
 
-@register(outgoing=True, pattern=r"^\.helpme")
+@kyy_cmd(pattern="helpme(?: |$)(.*)")
 async def yardim(event):
     try:
         tgbotusername = BOT_USERNAME
