25560f58bf140659bad7f08f5b46630159a0db21
muhammadrizky16 <malikkudus369@gmail.com>
2022-02-12 03:39:20 +0000

    pylint: auto fixes
    Signed-off-by: muhammadrizky16 <malikkudus369@gmail.com>

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -10,7 +10,7 @@ from importlib import import_module
 from telethon.tl.functions.channels import InviteToChannelRequest
 
 from telethon.errors.rpcerrorlist import PhoneNumberInvalidError
-from userbot import BOTLOG_CHATID,BOT_USERNAME, BOT_TOKEN, BOT_VER, LOGS, ALIVE_NAME, bot
+from userbot import BOTLOG_CHATID, BOT_USERNAME, BOT_TOKEN, BOT_VER, LOGS, ALIVE_NAME, bot
 
 from userbot.modules import ALL_MODULES
 from userbot.utils import autobot
