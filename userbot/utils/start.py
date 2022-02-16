5210d5534bde12c80ed894644d3a63de7615f34e
ᴷʸʸ <89422473+muhammadrizky16@users.noreply.github.com>
2022-02-17 04:44:57 +0700

    y maap

--- userbot/utils/start.py
+++ userbot/utils/start.py
@@ -11,7 +11,8 @@ async def startupmessage():
         if BOTLOG:
             await tgbot.send_file(
                 BOTLOG_CHATID,
-                "✨ **Kyy Userbot Has Been Actived**!!\n━━━━━━━━━━━━━━━\n➠ **Userbot Version** - 8.0@master\n━━━━━━━━━━━━━━━\n➠ **Powered By:** @NastyProject ",
+                "https://telegra.ph/file/02f87cca391f9b9d627d5.jpg",
+                caption="✨ **Kyy Userbot Has Been Actived**!!\n━━━━━━━━━━━━━━━\n➠ **Userbot Version** - 8.0@master\n━━━━━━━━━━━━━━━\n➠ **Powered By:** @NastyProject ",
                 buttons=[(Button.url("ꜱᴜᴘᴘᴏʀᴛ", "https://t.me/NastySupportt"),)],
             )
     except Exception as e:
