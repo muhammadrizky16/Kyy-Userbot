9eab095d4e576f2b6ca45ab197f0c604e095921b
ᴷʸʸ <89422473+muhammadrizky16@users.noreply.github.com>
2022-02-17 03:47:56 +0700

    Update utils.py

--- userbot/utils/utils.py
+++ userbot/utils/utils.py
@@ -1,3 +1,7 @@
+# Credits: @mrismanaziz
+# FROM Man-Userbot <https://github.com/mrismanaziz/Man-Userbot>
+# t.me/SharingUserbot & t.me/Lunatic0de
+
 import asyncio
 import importlib
 import logging
@@ -32,7 +36,7 @@ async def autobot():
         return
     await bot.start()
     await bot.send_message(
-        BOTLOG_CHATID, "➕ **Sedang Membuat Bot Di @BotFather Harap Tunggu Beberapa Detik!**"
+        BOTLOG_CHATID, "**SEDANG MEMBUAT BOT TELEGRAM UNTUK ANDA DI @BotFather**"
     )
     who = await bot.get_me()
     name = who.first_name + " Assistant Bot"
@@ -49,9 +53,9 @@ async def autobot():
     await bot.send_message(bf, "/newbot")
     await asyncio.sleep(1)
     isdone = (await bot.get_messages(bf, limit=1))[0].text
-    if isdone.startswith("Itu tidak bisa saya lakukan."):
+    if isdone.startswith("That I cannot do."):
         LOGS.info(
-            "🚧 Silakan buat Bot dari @BotFather dan tambahkan tokennya di var BOT_TOKEN"
+            "Silakan buat Bot dari @BotFather dan tambahkan tokennya di var BOT_TOKEN"
         )
         sys.exit(1)
     await bot.send_message(bf, name)
@@ -78,6 +82,12 @@ async def autobot():
         nowdone = (await bot.get_messages(bf, limit=1))[0].text
         if nowdone.startswith("Done!"):
             token = nowdone.split("`")[1]
+            await bot.send_message(bf, "/setinline")
+            await asyncio.sleep(1)
+            await bot.send_message(bf, f"@{username}")
+            await asyncio.sleep(1)
+            await bot.send_message(bf, "Search")
+            await asyncio.sleep(3)
             await bot.send_message(bf, "/setuserpic")
             await asyncio.sleep(1)
             await bot.send_message(bf, f"@{username}")
@@ -95,7 +105,7 @@ async def autobot():
             await bot.send_message(bf, f"@{username}")
             await asyncio.sleep(1)
             await bot.send_message(
-                bf, f"✨ Owner ~ {who.first_name} ✨\n\n✨ Powered By ~ @NastySupportt ✨"
+                bf, f"✨ Owner ~ {who.first_name} ✨\n\n✨ Powered By ~ @NastyProject ✨"
             )
             await bot.send_message(
                 BOTLOG_CHATID,
@@ -109,11 +119,17 @@ async def autobot():
             heroku_var["BOT_USERNAME"] = f"@{username}"
         else:
             LOGS.info(
-                "🚧 Silakan Hapus Beberapa Bot Telegram Anda di @Botfather atau Set Var BOT_TOKEN dengan token bot"
+                "Silakan Hapus Beberapa Bot Telegram Anda di @Botfather atau Set Var BOT_TOKEN dengan token bot"
             )
             sys.exit(1)
     elif isdone.startswith("Done!"):
         token = isdone.split("`")[1]
+        await bot.send_message(bf, "/setinline")
+        await asyncio.sleep(1)
+        await bot.send_message(bf, f"@{username}")
+        await asyncio.sleep(1)
+        await bot.send_message(bf, "Search")
+        await asyncio.sleep(3)
         await bot.send_message(bf, "/setuserpic")
         await asyncio.sleep(1)
         await bot.send_message(bf, f"@{username}")
@@ -131,7 +147,7 @@ async def autobot():
         await bot.send_message(bf, f"@{username}")
         await asyncio.sleep(1)
         await bot.send_message(
-            bf, f"✨ Owner ~ {who.first_name} ✨\n\n✨ Powered By ~ @NastySupportt ✨"
+            bf, f"✨ Owner ~ {who.first_name} ✨\n\n✨ Powered By ~ @NastyProject ✨"
         )
         await bot.send_message(
             BOTLOG_CHATID,
@@ -214,7 +230,6 @@ def remove_plugin(shortname):
                     del bot._event_builders[i]
     except BaseException:
         raise ValueError
-
 
 async def create_supergroup(group_name, client, botusername, descript):
     try:
