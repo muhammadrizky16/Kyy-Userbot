7c9693d54d793829e1520a6c818b109ad474f0ee
ᴷʸʸ <89422473+muhammadrizky16@users.noreply.github.com>
2022-02-27 11:26:19 +0700

    Update carbon.py

--- userbot/modules/carbon.py
+++ userbot/modules/carbon.py
@@ -9,7 +9,8 @@ from userbot import CMD_HANDLER as cmd
 from userbot import CMD_HELP
 from userbot.utils import edit_delete, edit_or_reply, kyy_cmd
 from userbot.utils.misc import Carbon
-from userbot.modules.vcg import vcmention
+
+from .vcg import vcmention
 
 all_col = [
     "Black",
