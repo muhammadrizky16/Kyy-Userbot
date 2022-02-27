c8e8b19bf06ee069a80237d7cf6fc7459305e8d1
ᴷʸʸ <89422473+muhammadrizky16@users.noreply.github.com>
2022-02-27 11:25:21 +0700

    hm

--- userbot/modules/vcg.py
+++ userbot/modules/vcg.py
@@ -7,6 +7,7 @@ from telethon.tl.functions.phone import CreateGroupCallRequest as startvc
 from telethon.tl.functions.phone import DiscardGroupCallRequest as stopvc
 from telethon.tl.functions.phone import GetGroupCallRequest as getvc
 from telethon.tl.functions.phone import InviteToGroupCallRequest as invitetovc
+from telethon.utils import get_display_name
 
 from userbot import CMD_HELP, CMD_HANDLER as cmd
 from userbot.utils import kyy_cmd
