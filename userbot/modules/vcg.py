0383134f1e72acd9660a97a494d001ea6979b6c3
ᴷʸʸ <89422473+muhammadrizky16@users.noreply.github.com>
2022-02-27 11:33:03 +0700

    fixed carbon

--- userbot/modules/vcg.py
+++ userbot/modules/vcg.py
@@ -7,6 +7,8 @@ from telethon.tl.functions.phone import CreateGroupCallRequest as startvc
 from telethon.tl.functions.phone import DiscardGroupCallRequest as stopvc
 from telethon.tl.functions.phone import GetGroupCallRequest as getvc
 from telethon.tl.functions.phone import InviteToGroupCallRequest as invitetovc
+
+from telethon.tl import types
 from telethon.utils import get_display_name
 
 from userbot import CMD_HELP, CMD_HANDLER as cmd
