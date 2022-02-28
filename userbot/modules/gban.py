cdcba542269ca2027724319c800b97785d44b4eb
ᴷʸʸ <89422473+muhammadrizky16@users.noreply.github.com>
2022-02-28 20:37:19 +0700

    fixes

--- userbot/modules/gban.py
+++ userbot/modules/gban.py
@@ -2,7 +2,8 @@ from telethon.events import ChatAction
 from telethon.tl.functions.contacts import BlockRequest, UnblockRequest
 from telethon.tl.types import MessageEntityMentionName
 
-from userbot import ALIVE_NAME, CMD_HELP, DEVS, bot, CMD_HANDLER as cmd
+from userbot import ALIVE_NAME, CMD_HELP, DEVS, bot
+from userbot import CMD_HANDLER as cmd
 from userbot.events import register
 from userbot.utils import kyy_cmd
 
