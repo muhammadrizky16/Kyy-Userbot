7a00d5079c0184d6766749ca7bcea8ec0d2361dd
muhammadrizky16 <malikkudus369@gmail.com>
2022-03-02 09:05:00 +0000

    pylint: auto fixes
    Signed-off-by: muhammadrizky16 <malikkudus369@gmail.com>

--- userbot/modules/sudo.py
+++ userbot/modules/sudo.py
@@ -9,7 +9,7 @@ from telethon.tl.functions.users import GetFullUserRequest
 
 from userbot import CMD_HANDLER as cmd
 from userbot import CMD_HELP, HEROKU_API_KEY, HEROKU_APP_NAME, SUDO_HANDLER, SUDO_USERS
-from userbot.utils import edit_delete, edit_or_reply, man_cmd
+from userbot.utils import edit_delete, edit_or_reply
 
 Heroku = heroku3.from_key(HEROKU_API_KEY)
 heroku_api = "https://api.heroku.com"
@@ -29,7 +29,7 @@ async def sudo(event):
         await edit_delete(event, "🔮 **Sudo:** `Disabled`")
 
 
-@kyy_cmd(pattern="addsudo(?:\s|$)([\s\S]*)")
+@kyy_cmd(pattern="addsudo(?:\\s|$)([\\s\\S]*)")
 async def add(event):
     suu = event.text[9:]
     if f"{cmd}add " in event.text:
@@ -73,7 +73,7 @@ async def add(event):
     heroku_Config[var] = newsudo
 
 
-@kyy_cmd(pattern="delsudo(?:\s|$)([\s\S]*)")
+@kyy_cmd(pattern="delsudo(?:\\s|$)([\\s\\S]*)")
 async def _(event):
     if event.sender_id in SUDO_USERS:
         return
