e9db1ebbaa0fe7bc13049eb411758a62bd8a2e94
muhammadrizky16 <malikkudus369@gmail.com>
2022-03-02 14:32:20 +0700

    try

--- userbot/modules/kyymisc.py
+++ userbot/modules/kyymisc.py
@@ -9,7 +9,7 @@ import html
 from justwatch import JustWatch
 from telethon import *
 from userbot.utils import kyy_cmd
-from userbot import CMD_HELP, bot, TEMP_DOWNLOAD_DIRECTORY, DEFAULT_BIO, ALIVE_NAME, CMD_HANDLER as cmd
+from userbot import CMD_HELP, bot, TEMP_DOWNLOAD_DIRECTORY, CMD_HANDLER as cmd
 from telethon import events
 from telethon.tl import functions
 from urllib.parse import quote
@@ -489,65 +489,6 @@ async def _(event):
             await event.client.delete_message(chat, event.chat_id, response.message)
 
 
-@kyy_cmd(pattern="clone(?: |$)(.*)")
-async def _(event):
-    if event.fwd_from:
-        return
-    reply_message = await event.get_reply_message()
-    replied_user, error_i_a = await get_full_user(event)
-    if replied_user is None:
-        await event.edit(str(error_i_a))
-        return False
-    user_id = replied_user.user.id
-    profile_pic = await event.client.download_profile_photo(user_id, TEMP_DOWNLOAD_DIRECTORY)
-    # some people have weird HTML in their names
-    first_name = html.escape(replied_user.user.first_name)
-    # https://stackoverflow.com/a/5072031/4723940
-    # some Deleted Accounts do not have first_name
-    if first_name is not None:
-        # some weird people (like me) have more than 4096 characters in their
-        # names
-        first_name = first_name.replace("\u2060", "")
-    last_name = replied_user.user.last_name
-    # last_name is not Manadatory in @Telegram
-    if last_name is not None:
-        last_name = html.escape(last_name)
-        last_name = last_name.replace("\u2060", "")
-    if last_name is None:
-        last_name = "⁪⁬⁮⁮⁮⁮ ‌‌‌‌"
-    # inspired by https://telegram.dog/afsaI181
-    user_bio = replied_user.about
-    if user_bio is not None:
-        user_bio = html.escape(replied_user.about)
-    await bot(functions.account.UpdateProfileRequest(
-        first_name=first_name
-    ))
-    await bot(functions.account.UpdateProfileRequest(
-        last_name=last_name
-    ))
-    await bot(functions.account.UpdateProfileRequest(
-        about=user_bio
-    ))
-    pfile = await bot.upload_file(profile_pic)  # pylint:disable=E060
-    await bot(functions.photos.UploadProfilePhotoRequest(  # pylint:disable=E0602
-        pfile
-    ))
-    #message_id_to_reply = event.message.reply_to_msg_id
-    # if not message_id_to_reply:
-    #    message_id_to_reply = event.message.id
-    # await bot.send_message(
-    #  event.chat_id,
-    #  "Hai, Apa Kabarmu?",
-    #  reply_to=message_id_to_reply,
-    #  )
-    await event.delete()
-    await bot.send_message(
-        event.chat_id,
-        "`Aku adalah kamu dan kamu adalah aku.!.`",
-        reply_to=reply_message
-    )
-
-
 async def get_full_user(event):
     if event.reply_to_msg_id:
         previous_message = await event.get_reply_message()
@@ -1016,18 +957,6 @@ async def xcursive(cursivelite):
     await cursivelite.edit(string)
 
 
-@kyy_cmd(pattern="rclone(?: |$)(.*)")
-async def _(event):
-    if event.fwd_from:
-        return
-    name = f"{ALIVE_NAME}"
-    bio = f"{DEFAULT_BIO}"
-    n = 1
-    await bot(functions.photos.DeletePhotosRequest(await event.client.get_profile_photos("me", limit=n)))
-    await bot(functions.account.UpdateProfileRequest(about=bio))
-    await bot(functions.account.UpdateProfileRequest(first_name=name))
-    await event.edit(f"`{ALIVE_NAME} Berhasil Mengembalikan Akun Anda dari clone.`")
-
 CMD_HELP.update({
     "misc":
     f"𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `{cmd}app`\
@@ -1061,14 +990,3 @@ Jika Tidak Memberikan Level Otomatis Default Ke Level 2\
 \n↳ : Memberikan Glitch (Gif , Stickers , Gambar, Video) Ke Sticker Dan Level Glitch 1 to 8.\
 Jika Tidak Memberikan Level Otomatis Default Ke Level 2."
 })
-
-
-CMD_HELP.update(
-    {
-        "clone": f"𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `{cmd}clone` <username>.\
-        \n↳ : Mulai Mengaktifkan Clonning Ke Seseorang\
-        \n\n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `'{cmd}rclone' Untuk Mengembalikan\
-        \n↳ : Mengembalikan Kloning, Dan Kembali Keakun Utama.\
-    "
-    }
-)
