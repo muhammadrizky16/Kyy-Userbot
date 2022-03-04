3087774004980d5af92e6d7c3c720beaf5e8f690
ᴷʸʸ <89422473+muhammadrizky16@users.noreply.github.com>
2022-03-05 02:46:07 +0700

    Update surat.py

--- userbot/modules/surat.py
+++ userbot/modules/surat.py
@@ -1,30 +1,30 @@
 from time import sleep
 
 from userbot import CMD_HELP, CMD_HANDLER as cmd
-from userbot.utils import kyy_cmd
+from userbot.utils import edit_or_reply, kyy_cmd
 
 
 @kyy_cmd(pattern='alfatihah(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
     sleep(1)
-    await typew.edit("**SURAT ALFATIHAH**")
+    xnxx = await edit_or_reply(typew, "**SURAT ALFATIHAH**")
     sleep(1)
-    await typew.edit("**bismillāhir-raḥmānir-raḥīm**")
+    await xnxx.edit("**bismillāhir-raḥmānir-raḥīm**")
     sleep(1)
-    await typew.edit("**al-ḥamdu lillāhi rabbil-'ālamīn**")
+    await xnxx.edit("**al-ḥamdu lillāhi rabbil-'ālamīn**")
     sleep(1)
-    await typew.edit("**ar-raḥmānir-raḥīm**")
+    await xnxx.edit("**ar-raḥmānir-raḥīm**")
     sleep(1)
-    await typew.edit("**māliki yaumid-dīn**")
+    await xnxx.edit("**māliki yaumid-dīn**")
     sleep(1)
-    await typew.edit("**iyyāka na'budu wa iyyāka nasta'īn**")
+    await xnxx.edit("**iyyāka na'budu wa iyyāka nasta'īn**")
     sleep(1)
-    await typew.edit("**ihdinaṣ-ṣirāṭal-mustaqīm**")
+    await xnxx.edit("**ihdinaṣ-ṣirāṭal-mustaqīm**")
     sleep(1)
-    await typew.edit("**ṣirāṭallażīna an'amta 'alaihim gairil-magḍụbi 'alaihim wa laḍ-ḍāllīn**")
+    await xnxx.edit("**ṣirāṭallażīna an'amta 'alaihim gairil-magḍụbi 'alaihim wa laḍ-ḍāllīn**")
     sleep(1)
-    await typew.edit("**Amin..**")
+    await xnxx.edit("**Amin..**")
 # Create by myself @localheart
 
 CMD_HELP.update({
