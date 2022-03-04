9f56bc124ce574a269e4e5516f205e3f5e9316eb
ᴷʸʸ <89422473+muhammadrizky16@users.noreply.github.com>
2022-03-05 03:17:09 +0700

    Update get_user_id.py

--- userbot/modules/get_user_id.py
+++ userbot/modules/get_user_id.py
@@ -1,6 +1,6 @@
 from telethon import events
 from telethon.errors.rpcerrorlist import YouBlockedUserError
-from userbot.utils import kyy_cmd
+from userbot.utils import edit_or_reply, edit_delete, kyy_cmd
 from userbot import bot, CMD_HELP, CMD_HANDLER as cmd
 
 
@@ -9,18 +9,18 @@ async def _(event):
     if event.fwd_from:
         return
     if not event.reply_to_msg_id:
-        await event.edit("`Mohon Reply Ke Pesan`")
+        await edit_delete(event, "`Mohon Reply Ke Pesan`")
         return
     reply_message = await event.get_reply_message()
     if not reply_message.text:
-        await event.edit("```Mohon Balas Ke Reply```")
+        await edit_delete(event, "```Mohon Balas Ke Reply```")
         return
     chat = "@getidsbot"
     reply_message.sender
     if reply_message.sender.bot:
-        await event.edit("`Mohon Reply Ke Pesan`")
+        await edit_delete(event, "`Mohon Reply Ke Pesan`")
         return
-    await event.edit("`Mencari ID.......`")
+    xx = await edit_or_reply(event, "`Mencari ID.......`")
     async with bot.conversation(chat) as conv:
         try:
             response = conv.wait_event(
@@ -33,9 +33,9 @@ async def _(event):
             await event.reply("`Bot Sedang Error`")
             return
         if response.text.startswith("Forward"):
-            await event.edit("`Mohon Maaf, Orang Ini Tidak Mempunyai ID`")
+            await xx.edit("`Mohon Maaf, Orang Ini Tidak Mempunyai ID`")
         else:
-            await event.edit(f"{response.message.message}")
+            await xx.edit(f"{response.message.message}")
 
 
 CMD_HELP.update({
