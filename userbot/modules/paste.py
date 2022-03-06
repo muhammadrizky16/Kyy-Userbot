736ce78b9c6f9c7c830d4a18afcf0ad786eb86b0
muhammadrizky16 <malikkudus369@gmail.com>
2022-03-06 09:43:53 +0000

    pylint: auto fixes
    Signed-off-by: muhammadrizky16 <malikkudus369@gmail.com>

--- userbot/modules/paste.py
+++ userbot/modules/paste.py
@@ -13,7 +13,7 @@ from userbot.utils import edit_delete, edit_or_reply, kyy_cmd
 from userbot.utils.pastebin import PasteBin
 
 
-@kyy_cmd(pattern="paste(?: (-d|-n|-h|-k|-s)|$)?(?: ([\s\S]+)|$)")
+@kyy_cmd(pattern="paste(?: (-d|-n|-h|-k|-s)|$)?(?: ([\\s\\S]+)|$)")
 async def paste(pstl):
     """For .paste command, pastes the text directly to a pastebin."""
     service = pstl.pattern_match.group(1)
