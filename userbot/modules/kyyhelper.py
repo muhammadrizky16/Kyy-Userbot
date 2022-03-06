08eddc8551dbba8133021c0f7c2fed4ee768916b
ᴷʸʸ <89422473+muhammadrizky16@users.noreply.github.com>
2022-03-06 16:58:11 +0700

    gpp

--- userbot/modules/kyyhelper.py
+++ userbot/modules/kyyhelper.py
@@ -27,7 +27,7 @@ async def var(m):
 CMD_HELP.update({
     "helper":
     "`.lhelp`\
-\nUsage: Bantuan Untuk Rose-Userbot.\
+\nUsage: Bantuan Untuk Kyy-Userbot.\
 \n`.vars`\
 \nUsage: Melihat Daftar Vars."
 })
