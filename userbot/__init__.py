217c8a334ce701b49141c079e0697ca63415c1b3
ᴷʸʸ <89422473+muhammadrizky16@users.noreply.github.com>
2022-03-09 18:29:34 +0700

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -599,7 +599,7 @@ with bot:
         @tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(rb"reopen")))
         async def on_plug_in_callback_query_handler(event):
             if event.query.user_id == uid or event.query.user_id in SUDO_USERS:
-                current_page_number = int(lockpage)
+                current_page_number = int(looters)
                 buttons = paginate_help(
                     current_page_number, dugmeler, "helpme")
                 text = f"**✨ Kyy-Userbot Inline Menu ✨**\n\n✣ **Owner** [{user.first_name}](tg://user?id={user.id})\n✣ **Jumlah** `{len(dugmeler)}` Modules"
