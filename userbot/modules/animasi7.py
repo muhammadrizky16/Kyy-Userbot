25fb9f38b257c6f8e8a5f9d6b87a43af0b87a0d0
ᴷʸʸ <89422473+muhammadrizky16@users.noreply.github.com>
2022-03-09 18:07:39 +0700

    Update animasi7.py

--- userbot/modules/animasi7.py
+++ userbot/modules/animasi7.py
@@ -1,13 +1,13 @@
 from time import sleep
 
 from userbot import CMD_HELP, CMD_HANDLER as cmd
-from userbot.utils import kyy_cmd
+from userbot.utils import edit_or_reply, kyy_cmd
 
 
 @kyy_cmd(pattern="sange$")
 async def koc(e):
     if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
-        await e.edit("SAYANGGGGGGGGG 💕")
+        e = await edit_or_reply(e, "SAYANGGGGGGGGG 💕")
         sleep(1)
         await e.edit("💝💘💓💗")
         sleep(1)
@@ -56,7 +56,7 @@ async def koc(e):
 @kyy_cmd(pattern="orgil(?: |$)(.*)")
 async def typewriter(typew):
     typew.pattern_match.group(1)
-    await typew.edit("`ANJING ADA ORANG GILA.....`")
+    typew = await edit_or_reply(typew, "`ANJING ADA ORANG GILA.....`")
     sleep(1)
     await typew.edit("`ORANG GILAAAAAA!!`")
     sleep(1)
@@ -114,7 +114,7 @@ async def typewriter(typew):
 @kyy_cmd(pattern="ass(?: |$)(.*)")
 async def typewriter(typew):
     typew.pattern_match.group(1)
-    await typew.edit("`Salam dulu biar sopan....`")
+    typew = await edit_or_reply(typew, "`Salam dulu biar sopan....`")
     sleep(2)
     await typew.edit("`A`")
     await typew.edit("`As`")
@@ -133,18 +133,17 @@ async def typewriter(typew):
     await typew.edit("`Assalamu'alaikum`")
     sleep(3)
     await typew.edit("**YANG GA JAWAB, FIX ATHEISS!!**")
-# Create by myself @ram-ubot
 
 
 CMD_HELP.update({
-    "animasi7": f"`{cmd}gabut` ; `{cmd}orgil`\
+    "animasi6": f"`{cmd}gabut` ; `{cmd}orgil`\
     \nUsage: ntahlah gabut doang.\
     \n\n`{cmd}ass`\
     \nUsage: Salam dulu biar sopan."
 })
 
 CMD_HELP.update({
-    "animasi8": f"`{cmd}cinta`\
+    "animasi7": f"`{cmd}cinta`\
     \nUsage: mengirim cintamu ke seseorang.\
     \n\n`{cmd}sange` :\
     \nUsage: cobalah sndiri ngentot."
