0fb74edb3589490a35807df1758a60d214a849e3
ᴷʸʸ <89422473+muhammadrizky16@users.noreply.github.com>
2022-03-09 18:11:44 +0700

    Update __help.py

--- userbot/modules/__help.py
+++ userbot/modules/__help.py
@@ -1,75 +1,53 @@
-# Copyright (C) 2020 TeamDerUntergang.
-#
-# SedenUserBot is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# SedenUserBot is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#    TeleBot - UserBot
+#    Copyright (C) 2020 TeleBot
 
-# @Qulec tarafından yazılmıştır.
-# Thanks @Spechide
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
 
-from telethon.errors.rpcerrorlist import BotInlineDisabledError as noinline
-from telethon.errors.rpcerrorlist import YouBlockedUserError
-from telethon.tl.functions.contacts import UnblockRequest
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
 
-from userbot import BOT_USERNAME, bot
-from userbot import CMD_HANDLER as cmd
-from userbot.utils import edit_or_reply, kyy_cmd
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-tgbotusername = BOT_USERNAME
+#    Recode by Fariz <Github.com/farizjs>
+#    From Flicks-Userbot
+#    <t.me/TheFlicksUserbot>
+
+import os
+
+from userbot import ALIVE_NAME, BOT_USERNAME, CMD_HELP, CMD_HANDLER, CMD_LIST, bot, tgbot
+from userbot.utils import kyy_cmd
+
+user = bot.get_me()
+DEFAULTUSER = user.first_name
+CUSTOM_HELP_EMOJI = "✨"
 
 
-@kyy_cmd(pattern="helpme$")
-async def yardim(event):
-    if event.fwd_from:
-        return
-    if tgbotusername is not None:
-        chat = "@Botfather"
+
+
+@flicks_cmd(pattern="help ?(.*)")
+async def cmd_list(event):
+    args = event.pattern_match.group(1).lower()
+    if args:
+        if args in CMD_HELP:
+            xx = await edit_or_reply(event, f"**✘ Commands available in {args} ✘** \n\n" + str(CMD_HELP[args]) + "\n\n**☞ @NastyProject**")
+        else:
+            await xx.edit(f"**Module** `{args}` **Tidak tersedia!**")
+    else:
         try:
-            results = await event.client.inline_query(tgbotusername, "@KyyUserbot")
+            results = await bot.inline_query(  # pylint:disable=E0602
+                BOT_USERNAME, "@KyyUserbot"
+            )
             await results[0].click(
                 event.chat_id, reply_to=event.reply_to_msg_id, hide_via=True
             )
-            await event.delete()
-        except noinline:
-            xx = await edit_or_reply(
-                event,
-                "**Inline Mode Tidak aktif.**\n__Sedang Menyalakannya, Harap Tunggu Sebentar...__",
+            await xx.delete()
+        except BaseException:
+            await xx.edit(
+                f"** Sepertinya obrolan atau bot ini tidak mendukung inline mode.**"
             )
-            async with bot.conversation(chat) as conv:
-                try:
-                    first = await conv.send_message("/setinline")
-                    second = await conv.get_response()
-                    third = await conv.send_message(BOT_USERNAME)
-                    fourth = await conv.get_response()
-                    fifth = await conv.send_message("Search")
-                    sixth = await conv.get_response()
-                    await bot.send_read_acknowledge(conv.chat_id)
-                except YouBlockedUserError:
-                    await event.client(UnblockRequest(chat))
-                    first = await conv.send_message("/setinline")
-                    second = await conv.get_response()
-                    third = await conv.send_message(BOT_USERNAME)
-                    fourth = await conv.get_response()
-                    fifth = await conv.send_message("Search")
-                    sixth = await conv.get_response()
-                    await bot.send_read_acknowledge(conv.chat_id)
-                await xx.edit(
-                    f"**Berhasil Menyalakan Mode Inline**\n\n**Ketik** `{cmd}helpme` **lagi untuk membuka menu bantuan.**"
-                )
-            await bot.delete_messages(
-                conv.chat_id,
-                [first.id, second.id, third.id, fourth.id, fifth.id, sixth.id],
-            )
-    else:
-        await edit_or_reply(
-            "**Silahkan Buat BOT di @BotFather dan Tambahkan Var** `BOT_TOKEN` & `BOT_USERNAME`",
-        )
