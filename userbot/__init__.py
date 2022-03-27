66c3c639976567a5e2cdc381d12a88993bde308f
ᴷʸʸ <89422473+muhammadrizky16@users.noreply.github.com>
2022-03-27 09:52:43 +0700

    fixes inline hell

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -823,7 +823,7 @@ with bot:
                 )
 
                 await event.edit(
-                    reply_pop_up_alert, buttons=[Button.inline("Back", data="open")]
+                    reply_pop_up_alert, buttons=[Button.inline("Back", data="reopen")]
                 )
             else:
                 reply_pop_up_alert = f"Kamu Tidak diizinkan, ini Userbot Milik {owner}"
