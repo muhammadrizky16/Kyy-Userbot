ffde94e6aef2fe43f0018b3c408c19fcac047859
ᴷʸʸ <89422473+muhammadrizky16@users.noreply.github.com>
2022-03-30 16:20:08 +0700

    Update utils.py

--- userbot/utils/utils.py
+++ userbot/utils/utils.py
@@ -134,7 +134,7 @@ async def autobot():
                 manage_call=True,
             )
             await bot(EditAdminRequest(int(BOTLOG_CHATID), f"@{username}", rights, "ᴀssɪsᴛᴀɴᴛ  ᴋʏʏ"))
-            kntl = "userbot/resources/IMG_20211216_160240_756.jpg"
+            kntl = "resources/extras/IMG_20211216_160240_756.jpg"
             await bot(EditPhotoRequest(BOTLOG_CHATID, await bot.upload_file(kntl)))
             heroku_var["BOT_TOKEN"] = token
             heroku_var["BOT_USERNAME"] = f"@{username}"
@@ -189,7 +189,7 @@ async def autobot():
             manage_call=True,
         )
         await bot(EditAdminRequest(int(BOTLOG_CHATID), f"@{username}", rights, "ᴀssɪsᴛᴀɴᴛ  ᴋʏʏ"))
-        kntl = "userbot/resources/IMG_20211216_160240_756.jpg"
+        kntl = "resources/extras/IMG_20211216_160240_756.jpg"
         await bot(EditPhotoRequest(BOTLOG_CHATID, await bot.upload_file(kntl)))
         heroku_var["BOT_TOKEN"] = token
         heroku_var["BOT_USERNAME"] = f"@{username}"
