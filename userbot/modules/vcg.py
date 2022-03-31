034d42bd25fd6ab1ad4c1c1ddccffcf25ce46df4
CoeF <57592261+CoeF@users.noreply.github.com>
2022-03-31 17:55:31 +0700

    ee

--- userbot/modules/vcg.py
+++ userbot/modules/vcg.py
@@ -57,16 +57,17 @@ async def start_voice(c):
 @kyy_cmd(pattern="stopvc$")
 @register(pattern=r"^\.stopvcs$", sudo=True)
 async def stop_voice(c):
+    me = await c.client.get_me()
     chat = await c.get_chat()
     admin = chat.admin_rights
     creator = chat.creator
 
     if not admin and not creator:
-        await edit_delete(c, f"**Maaf {owner} Bukan Admin 👮**")
+        await edit_delete(c, f"**Maaf {me.first_name} Bukan Admin 👮**")
         return
     try:
         await c.client(stopvc(await get_call(c)))
-        await edit_or_reply(c, "`Mematikan Obrolan Suara`")
+        await edit_or_reply(c, "`Voice Chat Stopped...`")
     except Exception as ex:
         await edit_delete(c, f"**ERROR:** `{ex}`")
 
