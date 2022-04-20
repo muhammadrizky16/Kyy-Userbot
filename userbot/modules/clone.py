42e5cc4e591510096c351cdf2f81fd7d9da99255
ᴷʸʸ <89422473+muhammadrizky16@users.noreply.github.com>
2022-04-21 02:52:14 +0700

    fixes clone

--- userbot/modules/clone.py
+++ userbot/modules/clone.py
@@ -9,50 +9,51 @@ from telethon.tl.functions.users import GetFullUserRequest
 from telethon.tl.types import InputPhoto
 
 from userbot import CMD_HANDLER as cmd
-from userbot import CMD_HELP, LOGS, STORAGE
-from userbot.utils import kyy_cmd
+from userbot import CMD_HELP, DEVS, LOGS, STORAGE
+from userbot.utils import edit_or_reply, kyy_cmd
 
 if not hasattr(STORAGE, "userObj"):
     STORAGE.userObj = False
 
 
-@kyy_cmd(pattern=r"clone ?(.*)")
+@kyy_cmd(pattern="clone ?(.*)", allow_sudo=False)
 async def impostor(event):
     inputArgs = event.pattern_match.group(1)
-
+    xnxx = await edit_or_reply(event, "`Processing...`")
     if "restore" in inputArgs:
         await event.edit("**Kembali ke identitas asli...**")
         if not STORAGE.userObj:
-            return await event.edit(
-                "**Anda harus mengclone orang dulu sebelum kembali!**"
-            )
-        await updateProfile(STORAGE.userObj, restore=True)
-        return await event.edit("**Berhasil Mengembalikan Akun Anda dari clone**")
+            return await xnxx.edit("**Anda harus mengclone orang dulu sebelum kembali!**")
+        await updateProfile(event, STORAGE.userObj, restore=True)
+        return await xnxx.edit("**Berhasil Mengembalikan Akun Anda dari clone**")
     if inputArgs:
         try:
             user = await event.client.get_entity(inputArgs)
         except BaseException:
-            return await event.edit("**Username/ID tidak valid.**")
+            return await xnxx.edit("**Username/ID tidak valid.**")
         userObj = await event.client(GetFullUserRequest(user))
     elif event.reply_to_msg_id:
         replyMessage = await event.get_reply_message()
+        if replyMessage.sender_id in DEVS:
+            return await xnxx.edit(
+                "**Tidak dapat menyamar sebagai developer kyy-userbot 😡**"
+            )
         if replyMessage.sender_id is None:
-            return await event.edit("**Tidak dapat menyamar sebagai admin anonim**")
+            return await xnxx.edit("**Tidak dapat menyamar sebagai admin anonim ☹️**")
         userObj = await event.client(GetFullUserRequest(replyMessage.sender_id))
     else:
-        return await event.edit("**Ketik** `.help impostor` **bila butuh bantuan.**")
+        return await xnxx.edit(f"**Ketik** `{cmd}help clone` **bila butuh bantuan.**")
 
     if not STORAGE.userObj:
         STORAGE.userObj = await event.client(GetFullUserRequest(event.sender_id))
 
     LOGS.info(STORAGE.userObj)
+    await xnxx.edit("**Mencuri identitas orang ini...**")
+    await updateProfile(event, userObj)
+    await xnxx.edit("**Aku adalah kamu dan kamu adalah aku. asekk 😜**")
 
-    await event.edit("**Mencuri identitas orang ini...**")
-    await updateProfile(userObj)
-    await event.edit("**Aku adalah kamu dan kamu adalah aku. asekk 🤪**")
 
-
-async def updateProfile(userObj, restore=False):
+async def updateProfile(event, userObj, restore=False):
     firstName = (
         "Deleted Account"
         if userObj.user.first_name is None
@@ -62,9 +63,9 @@ async def updateProfile(userObj, restore=False):
     userAbout = userObj.about if userObj.about is not None else ""
     userAbout = "" if len(userAbout) > 70 else userAbout
     if restore:
-        userPfps = await bot.get_profile_photos("me")
+        userPfps = await event.client.get_profile_photos("me")
         userPfp = userPfps[0]
-        await bot(
+        await event.client(
             DeletePhotosRequest(
                 id=[
                     InputPhoto(
@@ -78,11 +79,13 @@ async def updateProfile(userObj, restore=False):
     else:
         try:
             userPfp = userObj.profile_photo
-            pfpImage = await bot.download_media(userPfp)
-            await bot(UploadProfilePhotoRequest(await bot.upload_file(pfpImage)))
+            pfpImage = await event.client.download_media(userPfp)
+            await event.client(
+                UploadProfilePhotoRequest(await event.client.upload_file(pfpImage))
+            )
         except BaseException:
             pass
-    await bot(
+    await event.client(
         UpdateProfileRequest(about=userAbout, first_name=firstName, last_name=lastName)
     )
 
